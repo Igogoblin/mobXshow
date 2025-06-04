3ffc93883f5483b33a024ffb69ae3a74375b1489
Ihar Skavysh <igogoblin@gmail.com>
2025-06-04 09:21:26 +0300

    feat: add base in vite confige

--- src/pages/ghpages.tsx
+++ src/pages/ghpages.tsx
@@ -2,12 +2,18 @@ import { Typography } from "antd";
 
 const GitHabPages = () => {
   return (
-    <Typography.Title
-      level={2}
-      style={{ textAlign: "center", marginTop: "20px" }}
-    >
-      GH-Pages ACTIONS
-    </Typography.Title>
+    <>
+      <Typography.Title
+        level={2}
+        style={{ textAlign: "center", marginTop: "20px" }}
+      >
+        GH-Pages ACTIONS
+      </Typography.Title>
+      <Typography.Paragraph style={{ textAlign: "center", marginTop: "20px" }}>
+        This page is used to demonstrate GitHub Actions for deploying to GitHub
+        Pages.
+      </Typography.Paragraph>
+    </>
   );
 };
 export default GitHabPages;
