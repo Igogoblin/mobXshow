acf741cfd6c870e95d8a4bf7651e1dbeb47df905
Ihar Skavysh <igogoblin@gmail.com>
2025-06-06 22:02:37 +0300

    refactor: change instruments file

--- src/pages/instruments/instruments.tsx
+++ src/pages/instruments/instruments.tsx
@@ -1,6 +1,14 @@
 import { Typography } from "antd";
 
 const Instruments = () => {
-  return <Typography.Title level={2}>Инструменты</Typography.Title>;
+  return (
+    <>
+      <Typography.Title level={2}>Инструменты</Typography.Title>
+      <p>
+        скорее всего здесь лучше сделать пояснения за разные инструменты... или
+        удалить
+      </p>
+    </>
+  );
 };
 export default Instruments;
